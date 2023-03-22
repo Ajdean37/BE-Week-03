61dfee9d212831eb35e5142f4d00c9d58b1aac1a
Ajdean37 <ajdean37@gmail.com>
2023-03-21 20:51:28 -0500

    "Assignments"

--- src/Assignments.java
+++ src/Assignments.java
@@ -3,37 +3,92 @@ package src;
 public class Assignments {
 
   public static void main(String[] args) {
-    
-    int[] ages = new int[8];
+  
+//1: Create an array of int called ages that contains the following values: 3, 9, 23, 64, 2, 8, 28, 93.
 
-    ages[0] = 3;
-    ages[1] = 9;
-    ages[2] = 23;
-    ages[3] = 64;
-    ages[4] = 2;
-    ages[5] = 8;
-    ages[6] = 28;
-    ages[7] = 93;
+    int[] ages = {3, 9, 23, 64, 2, 8, 28, 93};
+
 
     for (int i = 0; i < ages.length; i++) {
-      System.out.println(i);
+      System.out.println(ages[i]);
     }
 
-  }
-  
+ // A: Programmatically subtract the value of the first element in the array from the value in the last element of the array (i.e. do not use ages[7] in your code). Print the result to the console. 
+
+    ages[ages.length-1] = ages[ages.length-1] - ages[0];
+    System.out.println(ages[ages.length-1]);
+
+// B: Add a new age to your array and repeat the step above to ensure it is dynamic (works for arrays of different lengths).
+
+    int[] newAges = new int[ages.length + 1];;
+    System.arraycopy(ages, 0, newAges, 0, ages.length);
+    newAges[newAges.length - 1] = 7;
+    System.out.println(newAges[newAges.length-1]);
+
+    newAges[newAges.length-1] = newAges[newAges.length-1] - newAges[0];
+    System.out.println(newAges[newAges.length-1]);
+
+// C: Use a loop to iterate through the array and calculate the average age. Print the result to the console.
+
+    int sum = 0;
+    int avg = 0;
+
+    for (int i = 0; i < newAges.length; i++) {
+      sum += newAges[i];
+    }
+    avg = sum / newAges.length;
+    System.out.println(avg);
+
+// 2: Create an array of String called names that contains the following values: “Sam”, “Tommy”, “Tim”, “Sally”, “Buck”, “Bob”.
+
+    String[] names = {"Sam", "Tommy", "Tim", "Sally", "Buck", "Bob"};
+
+// A: Use a loop to iterate through the array and calculate the average number of letters per name. Print the result to the console.
+
+    // int nameSum = 0;
+    // int nameAvg = 0;
+
+    // for (int i = 0; i < names.length; i++) {
+      
+    // }
+
+
+// B: Use a loop to iterate through the array again and concatenate all the names together, separated by spaces, and print the result to the console.
+
+String allNames = "";
+
+for (String name : names) {
+  allNames += name + " ";
 }
-/*
- 1: Create an array of int called ages that contains the following values: 3, 9, 23, 64, 2, 8, 28, 93.
-      A: Programmatically subtract the value of the first element in the array from the value in the last element of the array (i.e. do not use ages[7] in your code). Print the result to the console.  
-      B: Add a new age to your array and repeat the step above to ensure it is dynamic (works for arrays of different lengths).
-      C: Use a loop to iterate through the array and calculate the average age. Print the result to the console.
-2: Create an array of String called names that contains the following values: “Sam”, “Tommy”, “Tim”, “Sally”, “Buck”, “Bob”.
-      A: Use a loop to iterate through the array and calculate the average number of letters per name. Print the result to the console.
-      B: Use a loop to iterate through the array again and concatenate all the names together, separated by spaces, and print the result to the console.
-3: How do you access the last element of any array?
-4: How do you access the first element of any array?
-5: Create a new array of int called nameLengths. Write a loop to iterate over the previously created names array and add the length of each name to the nameLengths array.
-6: Write a loop to iterate over the nameLengths array and calculate the sum of all the elements in the array. Print the result to the console.
+  System.out.println(allNames);
+
+
+//3: How do you access the last element of any array?
+
+    System.out.println(names[names.length-1]);
+
+//4: How do you access the first element of any array?
+
+    System.out.println(names[0]);
+
+//5: Create a new array of int called nameLengths. Write a loop to iterate over the previously created names array and add the length of each name to the nameLengths array.
+
+  // int[] nameLengths = new int[names.length];
+
+  // for (int i = 0; i < names.length; i++) {
+    
+  // }
+
+//6: Write a loop to iterate over the nameLengths array and calculate the sum of all the elements in the array. Print the result to the console.
+
+    // for (int i = 0; i < names.length; i++) {
+      
+    // }
+
+  }
+}
+/* 
+
 7: Write a method that takes a String, word, and an int, n, as arguments and returns the word concatenated to itself n number of times. (i.e. if I pass in “Hello” and 3, I expect the method to return “HelloHelloHello”).
 8: Write a method that takes two Strings, firstName and lastName, and returns a full name (the full name should be the first and the last name as a String separated by a space).
 9: Write a method that takes an array of int and returns true if the sum of all the ints in the array is greater than 100.
